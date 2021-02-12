9934b71d009db9ef3c9aade5e89c6c06c5356343
Rahul Ratwatte <rahul.a.c.ratwatte@gmail.com>
2021-02-12 10:08:54 +0000

    moved sortAlgos from sortingVisualiser to new file

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { getMergeSortAnimations } from './sortingAlgorithms/getMergeSortAnimations';
+import { getQuickSortAnimations } from './sortingAlgorithms/getQuickSortAnimations';
+import { getSelectionSortAnimations } from './sortingAlgorithms/getSelectionSortAnimations';
+import { getBubbleSortAnimations } from './sortingAlgorithms/getBubbleSortAnimations';
+import { resetArray, bubbleSort, mergeSort, quickSort, selectionSort} from './sortingAlgorithms/sortingAlgorithms';
+import SortingVisualizer from './components/SortingVisualizer';
 
+ 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
